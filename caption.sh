e0ea4bb0261a3362f8a972a6639948c4780a940c
TJ Nicolaides <tjnicolaides@gmail.com>
2016-09-04 00:02:10 -0400

    Changing the script so only the source directory gets input. Destination is automatically written parallel to the source directory

--- caption.sh
+++ caption.sh
@@ -5,28 +5,27 @@ if [ -z "$1" ]
     exit 1
 fi
 
-if [ -z "$2" ]
-  then
-    echo "No destination supplied"
-    exit 1
-fi
-DEST="$2"
 COLOR="rgba(0,0,0,0.5)"
 FONT="/System/Library/Fonts/HelveticaNeue.dfont"
-STYLENAME="$(basename "$1")"
 
-[ -d "$DEST" ] || mkdir "$DEST"
 
 find "$1" -iname "*jpg" -name "*_*" | \
 	while read FILE; do
 		BASEFILENAME=$(basename "$FILE")
+		STYLENAME=$(echo $BASEFILENAME | awk -F'[-]' '{print $(1)}' | xargs);
+
+		DEST=$(dirname "$FILE")"_export"
+		[ -d "$DEST" ] || mkdir "$DEST"
+
+
 		echo $FILE 
 		echo "$DEST"/$BASEFILENAME
+
 		CLOTHINGSIZE=$(echo $BASEFILENAME | awk -F'[_.]' '{print $(NF-1)}');
-		echo $CLOTHINGSIZE;
 		SECONDLABEL=$STYLENAME;
 		[[ !  -z  $CLOTHINGSIZE  ]] && SECONDLABEL+=" - $CLOTHINGSIZE"
-		echo $SECONDLABEL
+
+		
 		width=`identify -format %w "$FILE"`; \
 		convert "$FILE" \
 		-fill $COLOR -font $FONT  -pointsize 50 \
